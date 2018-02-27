82f7201e2a054857b455d76a20fd979a0722ab4e
Kaila Goff and Ron Craig <ron@>
2018-02-27 11:57:41 -0800

    working on functionality

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require("sinatra")
+require("pry")
 require("sinatra/reloader")
 require("sinatra/activerecord")
 also_reload("lib/**/*.rb")
@@ -22,5 +23,35 @@ post('/employee') do
   employee = Employee.new({:name => name, :id => nil})
   employee.save()
   @employees = Employee.all()
+  erb(:employees)
+end
+
+get('/employee') do
+  @employees = Employee.all()
   erb(:employee)
+end
+
+get('/employees/:id') do
+  @employees =
+  @employees = Employee.find(params.fetch("id").to_i())
+  erb(:employee)
+end
+
+get('/employee/:id/edit') do
+  @employees = Employee.find(params.fetch("id").to_i())
+  erb(:employee_edit)
+end
+
+patch("/employee/:id") do
+  name = params.fetch("name")
+  @employees = Employee.find(params.fetch("id").to_i())
+  @employees.update({:name => name})
+  erb(:employee)
+end
+
+delete("/employee/:id") do
+  @employees = Employee.find(params.fetch("id").to_i())
+  @employees.delete()
+  @employees = Employee.all()
+  erb(:employees)
 end
