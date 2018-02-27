6a4d26eb84eb015cda7af2fe502d1de265ce7387
Kaila Goff and Ron Craig <kaila@>
2018-02-27 10:25:21 -0800

    pass first project spec

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,1 +1,8 @@
 require("spec_helper")
+
+describe(Project) do
+  it("validates the presence of a title") do
+    project = Project.new({:title => ""})
+    expect(project.save()).to(eq(false))
+  end
+end
